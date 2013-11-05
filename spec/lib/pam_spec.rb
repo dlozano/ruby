5c55ee6cba0486cc39fdb1f09b5614c7e9d863c6
gcohen <geremy@pubnub.com>
2013-11-05 15:27:32 -0800

    WIP - refactoring tests

--- spec/lib/pam_spec.rb
+++ spec/lib/pam_spec.rb
@@ -16,25 +16,29 @@ describe "PAM" do
     @auth_key = 'myauthkey'
     @channel = "mychannel"
     @message = "hello PAM world!"
+
     @err_callback = lambda { |x|
-      puts x }
+      puts "err callback: #{x}" }
+
+    @msg_callback = lambda { |x|
+      puts "msg callback: #{x}" }
+
+    @p = Pubnub.new(:uuid => "myuuid", :subscribe_key => @subscribe_key, :publish_key => @publish_key, :secret_key => @secret_key, :error_callback => @err_callback)
 
   end
 
   context "when an auth_key is provided" do
 
-    context "when http_sync is true" do
+    before do
+      @p.auth_key = @auth_key
+    end
 
-      before do
-        @p = Pubnub.new(:subscribe_key => @subscribe_key, :publish_key => @publish_key, :secret_key => @secret_key, :error_callback => @err_callback)
-
-      end
+    context "when http_sync is true (sync)" do
 
       context "when a publish is made" do
 
         it 'should provide the auth key in the url' do
           VCR.use_cassette('pam1', :record => :none) do
-            @p.auth_key = @auth_key
             response = @p.publish(:channel => @channel, :message => @message, :http_sync => true)
             response.request.params["auth"].should == @auth_key
           end
@@ -45,7 +49,6 @@ describe "PAM" do
 
         it 'should provide the auth key in the url' do
           VCR.use_cassette('pam4', :record => :none) do
-            @p.auth_key = @auth_key
             response = @p.history(:channel => @channel, :count => 10, :http_sync => true)
             response.request.params["auth"].should == @auth_key
           end
@@ -58,7 +61,6 @@ describe "PAM" do
         context "on the initial subscribe" do
           it 'should provide the auth key in the url' do
             VCR.use_cassette('pam5', :record => :none) do
-              @p.auth_key = @auth_key
               response = @p.subscribe(:channel => @channel, :http_sync => true)
               response.request.params["auth"].should == @auth_key
             end
@@ -69,7 +71,6 @@ describe "PAM" do
         context "on the initial and subsequent subscribe" do
           it 'should provide the auth key in the url' do
             VCR.use_cassette('pam6', :record => :none) do
-              @p.auth_key = @auth_key
               response1 = @p.subscribe(:channel => @channel, :http_sync => true)
               response2 = @p.subscribe(:channel => @channel, :http_sync => true)
 
@@ -77,26 +78,19 @@ describe "PAM" do
               response2.request.params["auth"].should == @auth_key
 
             end
-
           end
         end
-
       end
-
 
     end
 
-    context "when http_sync is false" do
+    context "when http_sync is false (async)" do
 
-      before do
-        @msg_callback = lambda { |x|
-          puts "async msg: #{x}" }
 
-        @p = Pubnub.new(:uuid => "myuuid", :subscribe_key => @subscribe_key, :publish_key => @publish_key, :secret_key => @secret_key, :error_callback => @err_callback)
 
-      end
 
       context "when a publish is made" do
+
         before do
           mock(@p).verify_operation('publish', {:ssl => nil, :cipher_key => nil, :publish_key => "pub-c-e72b633d-bb2f-42ba-8e98-69a9d3f7bdaa",
                                                 :subscribe_key => "sub-c-8e798456-4520-11e3-9b46-02ee2ddab7fe", :secret_key => "sec-c-ZjFjZmRhODMtM2E5Yi00N2ViLWJjYTktMjk2NmExOTQyMmYz",
@@ -112,8 +106,58 @@ describe "PAM" do
           end
         end
       end
+
+
+      context "when history is made" do
+
+        it 'should provide the auth key in the url' do
+          VCR.use_cassette('pam9', :record => :once) do
+            response = @p.history(:channel => @channel, :count => 10, :http_sync => false)
+            response.request.params["auth"].should == @auth_key
+          end
+        end
+      end
+
+
+      #context "when subscribe is made" do
+      #
+      #  context "on the initial subscribe" do
+      #    it 'should provide the auth key in the url' do
+      #      VCR.use_cassette('pam8', :record => :once) do
+      #        response = @p.subscribe(:channel => @channel, :http_sync => false)
+      #        response.request.params["auth"].should == @auth_key
+      #      end
+      #    end
+      #  end
+      #
+      #
+      #  context "on the initial and subsequent subscribe" do
+      #    it 'should provide the auth key in the url' do
+      #      VCR.use_cassette('pam7', :record => :once) do
+      #        response1 = @p.subscribe(:channel => @channel, :http_sync => false)
+      #        response2 = @p.subscribe(:channel => @channel, :http_sync => false)
+      #
+      #        response1.request.params["auth"].should == @auth_key
+      #        response2.request.params["auth"].should == @auth_key
+      #
+      #      end
+      #    end
+      #  end
+      #end
+
+
     end
+
   end
+
+
+
+
+
+
+
+
+
 
 
   context "when an auth_key is not provided" do
@@ -123,6 +167,7 @@ describe "PAM" do
     end
 
     context "when http_sync is true" do
+
       context "when a publish is made" do
 
         it 'should not provide an auth key in the url' do
@@ -133,6 +178,8 @@ describe "PAM" do
         end
       end
     end
+
+
 
 
   end
