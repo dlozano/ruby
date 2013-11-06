f8c35653b6e83889e898d3df90f5386e1fa9db77
gcohen <geremy@pubnub.com>
2013-11-05 16:16:01 -0800

    completed async auth param tests

--- spec/lib/pam_spec.rb
+++ spec/lib/pam_spec.rb
@@ -117,38 +117,46 @@ describe "PAM" do
                                                 :error_callback => @err_callback, :channel => @channel, :count => 10, :http_sync => false, :callback => @msg_callback})
 
 
-          VCR.use_cassette('pam9', :record => :once) do
+          VCR.use_cassette('pam9', :record => :none) do
             @p.history(:channel => @channel, :count => 10, :http_sync => false, :callback => @msg_callback)
           end
         end
       end
 
+      context "when subscribe is made" do
 
-      #context "when subscribe is made" do
-      #
-      #  context "on the initial subscribe" do
-      #    it 'should provide the auth key in the url' do
-      #      VCR.use_cassette('pam8', :record => :once) do
-      #        response = @p.subscribe(:channel => @channel, :http_sync => false)
-      #        response.request.params["auth"].should == @auth_key
-      #      end
-      #    end
-      #  end
-      #
-      #
-      #  context "on the initial and subsequent subscribe" do
-      #    it 'should provide the auth key in the url' do
-      #      VCR.use_cassette('pam7', :record => :once) do
-      #        response1 = @p.subscribe(:channel => @channel, :http_sync => false)
-      #        response2 = @p.subscribe(:channel => @channel, :http_sync => false)
-      #
-      #        response1.request.params["auth"].should == @auth_key
-      #        response2.request.params["auth"].should == @auth_key
-      #
-      #      end
-      #    end
-      #  end
-      #end
+        context "on the initial subscribe" do
+          it 'should provide the auth key in the url' do
+
+            mock(@p).verify_operation('subscribe', {:ssl => nil, :cipher_key => nil, :publish_key => "pub-c-e72b633d-bb2f-42ba-8e98-69a9d3f7bdaa",
+                                                  :subscribe_key => "sub-c-8e798456-4520-11e3-9b46-02ee2ddab7fe", :secret_key => "sec-c-ZjFjZmRhODMtM2E5Yi00N2ViLWJjYTktMjk2NmExOTQyMmYz",
+                                                  :origin => "pubsub.pubnub.com", :operation => "subscribe", :params => {:uuid => "myuuid", :auth => "myauthkey"}, :timetoken => nil,
+                                                  :error_callback => @err_callback, :channel => @channel, :http_sync => false, :callback => @msg_callback})
+
+            VCR.use_cassette('pam8', :record => :none) do
+              @p.subscribe(:channel => @channel, :http_sync => false, :callback => @msg_callback)
+            end
+          end
+        end
+
+
+        context "on the initial and subsequent subscribe" do
+          it 'should provide the auth key in the url' do
+
+            mock(@p).verify_operation('subscribe', {:ssl => nil, :cipher_key => nil, :publish_key => "pub-c-e72b633d-bb2f-42ba-8e98-69a9d3f7bdaa",
+                                                    :subscribe_key => "sub-c-8e798456-4520-11e3-9b46-02ee2ddab7fe", :secret_key => "sec-c-ZjFjZmRhODMtM2E5Yi00N2ViLWJjYTktMjk2NmExOTQyMmYz",
+                                                    :origin => "pubsub.pubnub.com", :operation => "subscribe", :params => {:uuid => "myuuid", :auth => "myauthkey"}, :timetoken => nil,
+                                                    :error_callback => @err_callback, :channel => @channel, :http_sync => false, :callback => @msg_callback}).times(2)
+
+            VCR.use_cassette('pam7', :record => :none) do
+              @p.subscribe(:channel => @channel, :http_sync => false, :callback => @msg_callback)
+              @p.subscribe(:channel => @channel, :http_sync => false, :callback => @msg_callback)
+
+
+            end
+          end
+        end
+      end
 
 
     end
